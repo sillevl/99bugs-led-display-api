cb5e6d87dfd64924695196597fe55617f70ca7c7
Sille Van Landschoot <info@sillevl.be>
2018-10-23 20:38:08 +0200

    add router with json response

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
+#[macro_use]
+extern crate serde_json;
+
 extern crate iron;
+extern crate router;
+extern crate serde;
 
 use iron::prelude::*;
 use iron::status;
 use iron::mime::Mime;
+use router::Router;
+
+fn handler(req: &mut Request) -> IronResult<Response> {
+
+    let response = json!({"status": "OK"});
+    let body = serde_json::to_string(&response).unwrap();
+
+    let content_type = "application/json".parse::<Mime>().unwrap();
+    Ok(Response::with((content_type, status::Ok, body)))
+}
 
 fn main() {
-    Iron::new(|_: &mut Request| {
-        let content_type = "application/json".parse::<Mime>().unwrap();
+    let mut router = Router::new();
+    router.get("/", handler, "index");
+    router.post("/", handler, "index");
 
-        Ok(Response::with((content_type, status::Ok, "{ \"response\": \"Hello world!\" }")))
-    }).http("localhost:3000").unwrap();
+    Iron::new(router).http("localhost:3000").unwrap();
 }
