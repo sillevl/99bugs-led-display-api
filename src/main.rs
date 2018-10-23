878bb62aff4db0a7e4d90aa59929610ec31800b6
Sille Van Landschoot <info@sillevl.be>
2018-10-23 21:08:56 +0200

    add bodyparser for json

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,22 @@ extern crate serde_json;
 extern crate iron;
 extern crate router;
 extern crate serde;
+extern crate bodyparser;
 
 use iron::prelude::*;
 use iron::status;
 use iron::mime::Mime;
+use iron::request::*;
 use router::Router;
 
 fn handler(req: &mut Request) -> IronResult<Response> {
+
+    let json_body = req.get::<bodyparser::Json>();
+    match json_body {
+        Ok(Some(json_body)) => println!("Parsed body:\n{:?}", json_body),
+        Ok(None) => println!("No body"),
+        Err(err) => println!("Error: {:?}", err)
+    }
 
     let response = json!({"status": "OK"});
     let body = serde_json::to_string(&response).unwrap();
