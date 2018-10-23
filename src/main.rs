8742d2e548884bd62b8913140211b60783efb533
Sille Van Landschoot <info@sillevl.be>
2018-10-23 19:58:59 +0200

    return json response

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ extern crate iron;
 
 use iron::prelude::*;
 use iron::status;
+use iron::mime::Mime;
 
 fn main() {
     Iron::new(|_: &mut Request| {
-        Ok(Response::with((status::Ok, "Hello World!")))
+        let content_type = "application/json".parse::<Mime>().unwrap();
+
+        Ok(Response::with((content_type, status::Ok, "{ \"response\": \"Hello world!\" }")))
     }).http("localhost:3000").unwrap();
 }
