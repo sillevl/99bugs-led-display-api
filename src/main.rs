7b6e6b9b2e943e37cb7eb1bf38c56d5fa21728d2
Sille Van Landschoot <info@sillevl.be>
2019-02-18 16:54:10 +0100

    add better error handling

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ extern crate router;
 extern crate serde;
 extern crate bodyparser;
 extern crate image;
+extern crate driver_99bugs_display;
 
 use iron::prelude::*;
 use iron::status;
@@ -15,9 +16,9 @@ use iron::mime::Mime;
 use router::Router;
 use std::io::Read;
 use image::GenericImageView;
+use driver_99bugs_display::Display;
 
 mod http_parser;
-mod png_parser;
 
 pub const BODY_BUFFER: usize = 1024;
 
@@ -41,22 +42,34 @@ fn handler(request: &mut Request) -> IronResult<Response> {
         message: "Request handled successfully".to_owned(),
     };
     
-    http_parser::check_content_type(request).expect("Content-type is set to image/png");
-    http_parser::check_content_size(request).expect("Content-size is set");
+    if let Err(err) = http_parser::check_content_type(request) {
+        response.status = "error".to_string();
+        response.message = err.to_string();
+        return generate_response(response);
+    }
     
     let mut content: [u8; BODY_BUFFER] = [0x00; BODY_BUFFER];
     request.body.read(&mut content).unwrap();
-    png_parser::check_png_header(&content).expect("PNG header to be found in content");
 
-    let img = image::load_from_memory(&content).unwrap().to_rgb();
-    println!("dimensions {:?}", img.dimensions());
+    // let image = ;
+    match image::load_from_memory(&content) {
+        Ok(image) => {
+            let image = image.to_rgb();
+            println!("dimensions {:?}", image.dimensions());
 
-    // match content_type {
-    //     Ok(Some(body)) => println!("Read body:\n{}", body),
-    //     Ok(None) => println!("No body"),
-    //     Err(err) => println!("Error: {:?}", err)
-    // }
-
+            let mut display = Display::new("/dev/spidev0.0");
+            
+            display.write_frame(&image);
+            display.flush();
+            println!("Done...");
+        },
+        Err(err) => {
+            response.status = "error".to_string();
+            response.message = err.to_string();
+            return generate_response(response);
+        }
+    }
+    
     return generate_response(response);    
 }
 
